92559db193166a01aaa633e22849fab2e5b43ad5
MJU <marie.ju2012@outlook.com>
2020-01-20 21:02:21 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,12 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(index)
+  return index - 1
+end
+
+def update_array_at_with(array, index, value)
+  array[index] = value
+end
+
+# code your input_to_index and move method here!
